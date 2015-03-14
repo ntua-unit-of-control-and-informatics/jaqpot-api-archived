c650fdcb8165d149a23ec3e4105f1647549df379
hampos <hampos@me.com>
2015-03-14 15:51:49 +0200

    Created parseProteomics() method in ConjoinerService to parse studies that have PROTEOMICS_SECTION as endpoint category. Proteomics values are served by Ambit as a JSON String in a Result's textValue. This JSON contains pairs of Protein ID and Result, and are parsed by Jackson into Proteomics class that simply extends Map<String,Result> interface.

--- JaqpotCoreServices/src/main/java/org/jaqpot/core/service/data/ConjoinerService.java
+++ JaqpotCoreServices/src/main/java/org/jaqpot/core/service/data/ConjoinerService.java
@@ -16,13 +16,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
+import java.util.TreeMap;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.MediaType;
+import org.jaqpot.core.annotations.Jackson;
+import org.jaqpot.core.data.serialize.JSONSerializer;
 import org.jaqpot.core.data.serialize.JacksonJSONSerializer;
 import org.jaqpot.core.service.client.Util;
 import org.jaqpot.core.service.dto.bundle.BundleSubstances;
@@ -30,8 +35,10 @@ import org.jaqpot.core.service.dto.dataset.DataEntry;
 import org.jaqpot.core.service.dto.dataset.Dataset;
 import org.jaqpot.core.service.dto.dataset.Substance;
 import org.jaqpot.core.service.dto.study.Effect;
+import org.jaqpot.core.service.dto.study.Result;
 import org.jaqpot.core.service.dto.study.Studies;
 import org.jaqpot.core.service.dto.study.Study;
+import org.jaqpot.core.service.dto.study.proteomics.Proteomics;
 
 /**
  *
@@ -42,6 +49,9 @@ import org.jaqpot.core.service.dto.study.Study;
 @Stateless
 public class ConjoinerService {
 
+//    @Inject
+//    @Jackson
+//    JSONSerializer jsonSerializer;
     public Dataset prepareDataset(String bundleURI, String subjectId) {
 
         try {
@@ -76,9 +86,13 @@ public class ConjoinerService {
     public DataEntry createDataEntry(Studies studies) {
         DataEntry dataEntry = new DataEntry();
         Substance compound = new Substance();
-        Map<String, Object> values = new HashMap<>();
+        Map<String, Object> values = new TreeMap<>();
         for (Study study : studies.getStudy()) {
             compound.setURI(study.getOwner().getSubstance().getUuid());
+            if (study.getProtocol().getCategory().getCode().equals("PROTEOMICS_SECTION")) {
+                values.putAll(parseProteomics(study));
+                continue;
+            }
             for (Effect effect : study.getEffects()) {
                 JacksonJSONSerializer serializer = new JacksonJSONSerializer();
                 String name = effect.getEndpoint();
@@ -89,12 +103,12 @@ public class ConjoinerService {
                 String endpointcategory = study.getProtocol().getCategory().getCode();
                 List<String> guidelines = study.getProtocol().getGuideline();
                 String guideline = guidelines == null || guidelines.isEmpty() ? "" : guidelines.get(0);
-                String propertyURI = getRelativeURI(name, topcategory, endpointcategory, identifier, Boolean.TRUE, guideline);
+                StringJoiner propertyURIJoiner = getRelativeURI(name, topcategory, endpointcategory, identifier, guideline);
                 Object value = calculateValue(effect);
                 if (value == null) {
                     continue;
                 }
-                values.put(propertyURI, value);
+                values.put(propertyURIJoiner.toString(), value);
             }
         }
         dataEntry.setCompound(compound);
@@ -107,10 +121,61 @@ public class ConjoinerService {
         return effect.getResult().getLoValue() == null ? null : effect.getResult().getLoValue();
     }
 
+    public Map<String, Object> parseProteomics(Study study) {
+        Map<String, Object> values = new TreeMap<>();
+        study.getEffects().stream().findFirst().ifPresent(effect -> {
+            String textValue = effect.getResult().getTextValue();
+            JacksonJSONSerializer serializer = new JacksonJSONSerializer();
+            Proteomics proteomics = serializer.parse(textValue, Proteomics.class);
+            proteomics.entrySet().stream().forEach(entry -> {
+                String name = effect.getEndpoint();
+                String units = effect.getResult().getUnit();
+                String conditions = serializer.write(effect.getConditions());
+                String identifier = createHashedIdentifier(name, units, conditions);
+                String topcategory = study.getProtocol().getTopcategory();
+                String endpointcategory = study.getProtocol().getCategory().getCode();
+                List<String> guidelines = study.getProtocol().getGuideline();
+                String guideline = guidelines == null || guidelines.isEmpty() ? "" : guidelines.get(0);
+                StringJoiner propertyURIJoiner = getRelativeURI(name, topcategory, endpointcategory, identifier, guideline);
+                propertyURIJoiner.add(entry.getKey());
+                Object protValue = entry.getValue().getLoValue();
+                values.put(propertyURIJoiner.toString(), protValue);
+            });
+        });
+        return values;
+    }
+
+    public StringJoiner getRelativeURI(
+            String name,
+            String topcategory,
+            String endpointcategory,
+            String identifier,
+            String guideline) {
+        StringJoiner joiner = new StringJoiner("/");
+        try {
+            joiner.add("property");
+            joiner.add(URLEncoder.encode(topcategory == null ? "TOX" : topcategory, "UTF-8"));
+            joiner.add(URLEncoder.encode(endpointcategory == null ? "UNKNOWN_TOXICITY_SECTION" : endpointcategory, "UTF-8"));
+            joiner.add(URLEncoder.encode(name, "UTF-8"));
+            joiner.add(identifier);
+            joiner.add(URLEncoder.encode(UUID.nameUUIDFromBytes(guideline.getBytes()).toString(), "UTF-8"));
+            return joiner;
+        } catch (UnsupportedEncodingException ex) {
+            return new StringJoiner("/").add("property");
+        }
+    }
+
+    @Deprecated
     public String getRelativeURI(String name, String topcategory, String endpointcategory, String identifier, Boolean extendedURI, String guideline) {
         try {
             return String.format("/property/%s/%s%s%s/%s%s%s",
-                    URLEncoder.encode(topcategory == null ? "TOX" : topcategory, "UTF-8"), URLEncoder.encode(endpointcategory == null ? "UNKNOWN_TOXICITY_SECTION" : endpointcategory, "UTF-8"), name == null ? "" : "/", name == null ? "" : URLEncoder.encode(name, "UTF-8"), identifier, extendedURI ? "/" : "", extendedURI ? URLEncoder.encode(UUID.nameUUIDFromBytes(guideline.getBytes()).toString(), "UTF-8") : "");
+                    URLEncoder.encode(topcategory == null ? "TOX" : topcategory, "UTF-8"),
+                    URLEncoder.encode(endpointcategory == null ? "UNKNOWN_TOXICITY_SECTION" : endpointcategory, "UTF-8"),
+                    name == null ? "" : "/",
+                    name == null ? "" : URLEncoder.encode(name, "UTF-8"),
+                    identifier,
+                    extendedURI ? "/" : "",
+                    extendedURI ? URLEncoder.encode(UUID.nameUUIDFromBytes(guideline.getBytes()).toString(), "UTF-8") : "");
         } catch (UnsupportedEncodingException x) {
             return "/property";
         }
---
+++ JaqpotCoreServices/src/main/java/org/jaqpot/core/service/dto/study/proteomics/Proteomics.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jaqpot.core.service.dto.study.proteomics;
+
+import java.util.HashMap;
+import org.jaqpot.core.service.dto.study.Result;
+
+/**
+ *
+ * @author hampos
+ */
+public class Proteomics extends HashMap<String, Result> {
+
+}
