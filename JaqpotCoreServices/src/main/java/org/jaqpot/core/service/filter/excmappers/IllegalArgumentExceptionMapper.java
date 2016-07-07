8ccdd25a4c5ec872a651cd6970c3ab232131633c
hampos <hampos@me.com>
2016-07-07 19:45:39 +0300

    Added bad request exception mapper for IllegalArgumentException

---
+++ JaqpotCoreServices/src/main/java/org/jaqpot/core/service/filter/excmappers/IllegalArgumentExceptionMapper.java
@@ -0,0 +1,69 @@
+/*
+ *
+ * JAQPOT Quattro
+ *
+ * JAQPOT Quattro and the components shipped with it, in particular:
+ * (i)   JaqpotCoreServices
+ * (ii)  JaqpotAlgorithmServices
+ * (iii) JaqpotDB
+ * (iv)  JaqpotDomain
+ * (v)   JaqpotEAR
+ * are licensed by GPL v3 as specified hereafter. Additional components may ship
+ * with some other licence as will be specified therein.
+ *
+ * Copyright (C) 2014-2015 KinkyDesign (Charalampos Chomenidis, Pantelis Sopasakis)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Source code:
+ * The source code of JAQPOT Quattro is available on github at:
+ * https://github.com/KinkyDesign/JaqpotQuattro
+ * All source files of JAQPOT Quattro that are stored on github are licensed
+ * with the aforementioned licence.
+ */
+package org.jaqpot.core.service.filter.excmappers;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import org.jaqpot.core.model.ErrorReport;
+import org.jaqpot.core.model.factory.ErrorReportFactory;
+
+/**
+ *
+ * @author Charalampos Chomenidis
+ */
+@Provider
+public class IllegalArgumentExceptionMapper implements ExceptionMapper<IllegalArgumentException> {
+
+    private static final Logger LOG = Logger.getLogger(IllegalArgumentExceptionMapper.class.getName());
+
+    @Override
+    public Response toResponse(IllegalArgumentException exception) {
+
+        LOG.log(Level.FINE, "IllegalArgumentException exception caught", exception);
+
+        ErrorReport error = ErrorReportFactory.badRequest(exception, null);
+
+        return Response
+                .status(Response.Status.BAD_REQUEST)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+}
