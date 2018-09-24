2f0e4673b2d26e0ccd0c92397026726fe5a5b7bd
Angelos Valsamis <ang.valsamis@gmail.com>
2018-09-24 20:04:02 +0300

    Update Project Description

--- src/main/java/org/jaqpot/core/service/resource/SwaggerFilter.java
+++ src/main/java/org/jaqpot/core/service/resource/SwaggerFilter.java
@@ -22,7 +22,7 @@ import javax.enterprise.context.ApplicationScoped;
 @SwaggerDefinition(
         info = @Info(
                 title = "Jaqpot API",
-                description = "Jaqpot v4 (Quattro) is the 4th version of a YAQP, a RESTful web service which can be used to train machine learning models and use them to obtain toxicological predictions for given chemical compounds or engineered nano materials. The project is written in Java8 and JEE7.",
+                description = "Jaqpot v4 (Quattro) is the 4th version of a YAQP, a RESTful web platform which can be used to train machine learning models and use them to obtain toxicological predictions for given chemical compounds or engineered nano materials. Jaqpot v4 has integrated read-across, optimal experimental design, interlaboratory comparison, biokinetics and dose response modelling functionalities. The project is developed in Java8 and JEE7 by the <a href=http://www.chemeng.ntua.gr/labs/control_lab/> Unit of Process Control and Informatics in the School of Chemical Engineering </a> at the  <a href=https://www.ntua.gr/en/> National Technical University of Athens</a>.",
                 version = "4.0.3",
                 contact = @Contact(name = "Charalampos Chomenidis, Pantelis Sopasakis, Evangelia Anagnostopoulou, Angelos Valsamis, George Drakakis, Pantelis Karatzas, Georgia Tsiliki, Philip Doganis, Haralambos Sarimveis", email = "hampos@me.com", url = "https://github.com/KinkyDesign/jaqpot-web/issues")
         )
